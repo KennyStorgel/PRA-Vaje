ad6b880e8f1abbec7a17e3da0540152407a9d972
Kenny <kenny.storgel@gmail.com>
2025-03-20 11:35:01 +0100

    Vaje 4

---
+++ PRA-Vaje4/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace PRA_Vaje4
+{
+    internal class Program
+    {
+        //public static int c = 10; // globalna spremenljivka
+        static void Main(string[] args)
+        {
+            //int a = 5; // lokalna spremenljivka
+            Console.WriteLine(Naloga1(2,4));
+            char[] tabela = ['b', 'e', 's', 'e', 'd', 'a'];
+            Console.WriteLine(Naloga3(tabela, 'a'));
+            Console.WriteLine(Naloga5("To je nek niz!", 'e'));
+
+            List<string> seznam1 = new List<string>() {"ananas", "banana", "avto", "avion", "klub" };
+            List<string> seznam = Naloga7(seznam1, 'a');
+
+            //string niz1 = "to je niz";
+            string niz1 = "to je niz ";
+            
+            foreach (string niz in Naloga8(niz1)) 
+            {
+                Console.WriteLine(niz);
+            }
+
+            var par = Naloga9(seznam1);
+            Console.WriteLine(par.Item1);
+            Console.WriteLine(par.Item2);
+
+            //Console.WriteLine(par.beseda);
+            //Console.WriteLine(par.indeks);
+
+            (string niz2, int stevilo) = Naloga9(seznam1);
+
+        }
+
+        // n-terice (dvojice, trojice, četvorke)
+        // n-tuple
+        public static (string, int) Naloga9(List<string> seznam) 
+        {
+            string beseda = ""; // trenutno najdaljšo besedo
+            int indeks = -1;
+
+            for (int i = 0; i < seznam.Count(); i++) 
+            {
+                if (beseda.Length < seznam[i].Length) 
+                {
+                    beseda = seznam[i];
+                    indeks = i;
+                }
+            }
+
+            return (beseda, indeks);
+        }
+
+        // naredimo svojo Split metodo za presledke
+        public static List<string> Naloga8(string niz)
+        {
+            List<string> seznam = new List<string>();
+
+            string beseda = "";
+            foreach (char znak in niz) 
+            {
+                if (znak != ' ')
+                {
+                    beseda += znak;
+                }
+                else if(beseda != "")
+                {
+                    seznam.Add(beseda);
+                    beseda = "";
+                }
+            }
+            // "to je niz"
+            // "to je niz "
+            if (beseda != "")
+            {
+                seznam.Add(beseda);
+            }
+
+            return seznam;
+        }
+
+        public static List<string> Naloga7(List<string> seznam, char znak) 
+        {
+            List<string> novSeznam = new List<string>();
+
+            for (int i = 0; i < seznam.Count(); i++) 
+            {
+                // seznam - tip List<string>
+                // seznam[i] - tip string
+                // seznam[i][0] - tip char
+                if (seznam[i][0] == znak) 
+                {
+                    novSeznam.Add(seznam[i]);
+                }
+            }
+
+            return novSeznam;
+        }
+
+        public static int Naloga5(string niz, char znak) 
+        {
+            int stPojavitev = 0;
+            for (int i = 0; i < niz.Length; i++) 
+            {
+                if (niz[i] == znak) 
+                {
+                    stPojavitev++;
+                }
+            }
+            return stPojavitev;
+        }
+
+        public static bool Naloga3(char[] tabela, char znak) 
+        {
+            for (int i = 0; i < tabela.Length; i++) 
+            {
+                if (tabela[i] == znak)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // določiloVidnosti določiloStatičnosti tipMetode ImeMetode(argumenti)
+        public static int Naloga1(int a, int b) // argumenti 
+        {
+            int potenca = 1;
+            for (int i = 1; i <= b; i++) 
+            {
+                potenca *= a;
+            }
+            return potenca;
+        }
+
+    }
+}
---
+++ Sudoku/Program.cs
@@ -0,0 +1,159 @@
+﻿namespace Sudoku
+{
+    internal class Program
+    {
+        public static int[,] igralnoPolje = {
+                                                {0, 5, 0, 0, 0, 0, 3, 7, 0 },
+                                                {3, 0, 0, 0, 5, 1, 0, 8, 0 },
+                                                {0, 8, 0, 0, 9, 0, 0, 0, 1 },
+                                                {0, 0, 0, 0, 0, 0, 0, 0, 7 },
+                                                {8, 7, 6, 0, 0, 0, 2, 9, 5 },
+                                                {9, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                                {4, 0, 0, 0, 2, 0, 0, 1, 0 },
+                                                {0, 6, 0, 4, 7, 0, 0, 0, 2 },
+                                                {0, 3, 2, 0, 0, 0, 0, 6, 0 }
+                                            };
+
+        static void Main(string[] args)
+        {
+            while (JeKonec() == false) 
+            {
+                Izpis();
+                VnosVrednosti();
+            }
+            Console.WriteLine("Zmagali ste!");
+        }
+
+        public static bool JeKonec() 
+        {
+            for (int i = 0; i < 9; i++) 
+            {
+                for (int j = 0; j < 9; j++) 
+                {
+                    if (igralnoPolje[i, j] < 1 || igralnoPolje[i, j] > 9) 
+                    {
+                        return false; // nismo še končali
+                    }
+                }
+            }
+            return true; // vse je pravilno izpolnjeno
+        }
+
+        public static void VnosVrednosti() 
+        {
+            Console.WriteLine("Vnesi vrstico, stolpec in vrednost (locene le s presledkom)");
+            string vnos = Console.ReadLine();
+            string[] podatki = vnos.Split(' ');
+
+            int vrstica = int.Parse(podatki[0]);
+            int stolpec = int.Parse(podatki[1]);
+            int vrednost = int.Parse(podatki[2]);
+
+            // če ni konflikta vpiši vrednost
+            if (PreveriVnos(vrstica, stolpec, vrednost))
+            {
+                igralnoPolje[vrstica - 1, stolpec - 1] = vrednost;
+            }
+            else 
+            {
+                Console.WriteLine("Vrednost je že v stolpcu/vrstici/škatli");
+            }
+        }
+
+        // stolpec in vrstica med 1 in 9
+        public static bool PreveriVnos(int vrstica, int stolpec, int vrednost) 
+        {
+            // preveri stolpec
+            for (int i = 0; i < 9; i++) 
+            {
+                if (igralnoPolje[i, stolpec - 1] == vrednost) 
+                {
+                    return false;
+                }
+            }
+
+            // preveri vrstico
+            for (int i = 0; i < 9; i++)
+            {
+                if (igralnoPolje[vrstica - 1, i] == vrednost)
+                {
+                    return false;
+                }
+            }
+
+            // TODO: preveri 3x3 - vrstica (1-9) stolpec (1-9)
+            int k = -1; // vrstica za 3x3 škatle
+            int l = -1; // stolpec za 3x3 škatle
+            if (vrstica >= 1 && vrstica <= 3)
+            {
+                k = 0;
+            }
+            else if (vrstica >= 4 && vrstica <= 6) 
+            {
+                k = 1;
+            }
+            else if (vrstica >= 7 && vrstica <= 9)
+            {
+                k = 2;
+            }
+
+            if (stolpec >= 1 && stolpec <= 3)
+            {
+                l = 0;
+            }
+            else if (stolpec >= 4 && stolpec <= 6)
+            {
+                l = 1;
+            }
+            else if (stolpec >= 7 && stolpec <= 9)
+            {
+                l = 2;
+            }
+
+            for (int i = 3*k; i < 3*k + 3; i++) 
+            {
+                for (int j = 3 * l; j < 3 * l + 3; j++) 
+                {
+                    if (igralnoPolje[i, j] == vrednost)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /*
+         * S tem ukazom spremenimo barvo pisave v terminalu (konzoli)
+         * Console.ForegroundColor = ConsoleColor.Red;
+        */
+        public static void Izpis() 
+        {
+            for (int i = 0; i < 9; i++) 
+            {
+                for (int j = 0; j < 9; j++) 
+                {
+                    if (igralnoPolje[i, j] == 0)
+                    {
+                        Console.Write(". ");
+                    }
+                    else 
+                    {
+                        Console.Write(igralnoPolje[i, j] + " ");
+                    }
+                    if (j == 2 || j == 5) 
+                    {
+                        Console.Write("| ");
+                    }
+                }
+                Console.WriteLine();
+
+                if (i == 2 || i == 5) 
+                {
+                    Console.WriteLine("---------------------");
+                }
+            }
+        }
+    }
+}
